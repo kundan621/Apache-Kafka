acc0ecac9c32494bd222b0ba17d6f9debe31fe1e
kundan621 <kundan621@gmail.com>
2019-07-09 23:43:10 +0530

    commit

--- kafka-java-programming/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemo.java
+++ kafka-java-programming/src/main/java/com/github/simplesteph/kafka/tutorial1/ProducerDemo.java
@@ -1,7 +1,36 @@
 package com.github.simplesteph.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.print("hello world");
+        String bootStrapServers="127.0.0.1:9092";
+
+        //create producer properties
+        Properties properties =new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String,String> producer=new KafkaProducer<String, String>(properties);
+
+        //create producer record
+        ProducerRecord<String,String> record=new ProducerRecord<String, String>("first_topic","hello-world");
+        //send data
+        producer.send(record);
+
+        //flush else program will close befor even consumer recieves it
+        producer.flush();
+
+        producer.close();
+
     }
+
+
 }
